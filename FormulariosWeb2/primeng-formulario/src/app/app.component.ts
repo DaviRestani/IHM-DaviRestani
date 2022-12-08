e0de7443658933c0339ff64edad34abd41495a73
Davi Restani <74680808+DaviRestani@users.noreply.github.com>
2022-12-07 22:09:55 -0300

    commit formularioswebv5

--- FormulariosWeb2/primeng-formulario/src/app/app.component.ts
+++ FormulariosWeb2/primeng-formulario/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeng-formulario';
+
+  //campos do formulário
+  nome = '';
+  sobrenome='';
+
+  cadastrar(){
+    alert('Pessoa Fisica' + this.nome + ' cadastrada')
+  }
 }
+
+interface EstadoCivil {
+  chave: string;
+  valor: string;
+}
+
